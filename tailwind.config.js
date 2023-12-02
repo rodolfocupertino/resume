b245c2327a7f175b418149e8067c65166b8069b3
Rodolfo Cupertino <rodolfols@gmail.com>
2023-12-01 23:47:29 -0300

    update

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content : ["./index.html"],
+  media: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
